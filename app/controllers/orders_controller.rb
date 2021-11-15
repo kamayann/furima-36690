765803f68960f42df309bcf21851f836c42ab7ec
kamayann <2520.lfp@gmail.com>
2021-11-15 14:51:40 +0900

    商品購入機能（viewファイルの更新）

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,6 @@
 class OrdersController < ApplicationController
   def index
-  end
-
-  def new
-
+    @item = Item.find(params[:item_id])
   end
 
   def create
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-  resources :items 
-  resources :order, only: [:index, :new, :create]
+  resources :items do
+    resources :orders, only: [:index, :create]
+  end
 end
